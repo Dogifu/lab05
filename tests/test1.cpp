367ffcc08da057f865b314358c513b93637e2849
Dogifu <maximus2106@mail.ru>
2022-06-01 18:32:26 +0300

    updated

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,39 +1,65 @@
+#include <gtest/gtest.h>
+#include "gmock/gmock.h"
 #include <Transaction.h>
 #include <Account.h>
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 
-inline bool operator==(Account a, Account b) { return true; }
 
 class MockAccount: public Account{
-	public:
-	MockAccount(int id, int balance):Account(id, balance){}
-	MOCK_METHOD(void, Unlock, (), (override));
+public:
+    MockAccount(int id, int balance):Account(id, balance){}
+    MOCK_METHOD(void, Unlock, ());
 };
-
-//Тестирование класса Account
-
-TEST(Account, GetBalance){
-	MockAccount acc(1,500);
-	EXPECT_EQ(acc.Account::GetBalance(), 500);
+TEST(Account, Init){
+    MockAccount test(1,100);
+    EXPECT_EQ(test.GetBalance(),100);
+    EXPECT_EQ(test.id(),1);
 }
 
-TEST(Account, ChangeBalance) {
-	MockAccount acc(1, 500);
-	EXPECT_THROW(acc.Account::ChangeBalance(250), std::runtime_error);
-	acc.Account::Lock();
-	acc.Account::ChangeBalance(250);
-	EXPECT_EQ(acc.Account::GetBalance(), 750);
+class MockTransaction: public Transaction{
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
+};
+
+TEST(Account, GetBalance){
+MockAccount acc(1,100);
+EXPECT_EQ(acc.Account::GetBalance(), 100);
+}
+
+TEST(Account, ChangeBalance){
+MockAccount acc(0, 100);
+EXPECT_THROW(acc.Account::ChangeBalance(50), std::runtime_error);
+acc.Account::Lock();
+acc.Account::ChangeBalance(50);
+EXPECT_EQ(acc.Account::GetBalance(), 150);
+
 }
 
 TEST(Account, Lock) {
-	MockAccount acc(1, 500);
-	acc.Account::Lock();
-	EXPECT_THROW(acc.Account::Lock(), std::runtime_error);
+    MockAccount acc(15,213);
+    acc.Lock();
+    EXPECT_THROW(acc.Lock(), std::runtime_error);
 }
 
-TEST(Account, Unlock) {
-	MockAccount acc(1, 500);
-	EXPECT_CALL(acc, Unlock()).Times(1);
-	acc.Unlock();
+TEST(Account, Unlock){
+    MockAccount acc(0, 100);
+EXPECT_CALL(acc, Unlock()).Times(1);
+acc.Unlock();
 }
+
+TEST(Transaction, Make){
+MockAccount from_acc(15,17650);
+MockAccount to_acc(13, 18435);
+MockTransaction tr;
+EXPECT_THROW(tr.Make(from_acc,from_acc,0),std::logic_error);
+EXPECT_THROW(tr.Make(from_acc,to_acc,0),std::logic_error);
+EXPECT_THROW(tr.Make(from_acc,to_acc,-5),std::invalid_argument);
+}
+
+TEST(Transaction, SaveToDataBase){
+MockAccount from_acc(15,17650);
+MockAccount to_acc(13, 18435);
+MockTransaction tr;
+
+
+}
+
